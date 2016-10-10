abfac07754adb8cf029e9646cc69a131a992e4a7
Ubuntu <swaffork@oregonstate.edu>
2016-10-10 20:31:19 +0000

    Remove deprecated editTodo function from controller, now adding in user email

--- todo/client/js/controllers.js
+++ todo/client/js/controllers.js
@@ -37,7 +37,7 @@ todoApp.controller('TodoController', function($rootScope, $scope, todosFactory) 
   $scope.updateStatus = function($event, _id, i) {
     var cbk = $event.target.checked;
     var _t = $scope.todos[i];
-    if (_t.todo && _t.priority && _t.user && _t.userType) { // make sure all fields filled out
+    if (_t.priority && _t.userType) { 
       console.log('Checkbox!', _t.todo, _t.priority, _t.user, _t.userType);
       todosFactory.updateTodo({ // write to model
         _id: _id,
@@ -56,7 +56,7 @@ todoApp.controller('TodoController', function($rootScope, $scope, todosFactory) 
     }
   };
 
-  // EDIT USER ----------------------------------
+  // EDIT ----------------------------------
   // change our editing state to NOT editing,
   // and also log what changes are happening
   // TBC: test case! If we weren't able to edit, send an alert!
@@ -86,33 +86,10 @@ todoApp.controller('TodoController', function($rootScope, $scope, todosFactory) 
     }
   };
 
-  // DEPRECATED
-  // change our editing state to NOT editing,
-  // and also log what changes are happening
-  // TBC: test case! If we weren't able to edit, send an alert!
-  $scope.editTodo = function($event, i) {
-    if ($event.which == 13 && $event.target.value.trim()) {
-      var _t = $scope.todos[i];
-      todosFactory.updateTodo({
-        _id: _t._id,
-        todo: $event.target.value.trim(),
-        isCompleted: _t.isCompleted
-      }).then(function(data) {
-              console.log(data);
-        if (data.data.updatedExisting) {
-          _t.todo = $event.target.value.trim();
-          $scope.isEditable[i] = false;
-        } else {
-          alert('Oops something went wrong!');
-        }
-      });
-    }
-  };
-
   // If we simply want to get rid of a todo permanently, we call the factory's delete method
   // This removes the object from mongodb with matching id
   // When we get a response from the delete method, we change the scope's todo list
-  // to NOT include the delete todo with splice
+  // to NOT include the delete todo (with splice)
   // more at https://docs.mongodb.com/manual/tutorial/remove-documents/
   $scope.delete = function(i) {
     todosFactory.deleteTodo($scope.todos[i]._id).then(function(data) {
