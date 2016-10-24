e221a6d0ae61e1371de4864c4626ae26e49558e7
Kendra Swafford <kendra.swafford@hp.com>
2016-10-24 10:51:38 -0700

    PUT now only replaced updated fields, still have to fix invalid id to get, delete, and put

--- almanac/server.js
+++ almanac/server.js
@@ -101,12 +101,15 @@ app.get("/places/:id", function (req, res) {
 app.put("/places/:id", function (req, res) {
     var updateDoc = req.body;
     delete updateDoc._id;
-
-    db.collection(PLACES_COLLECTION).updateOne({ _id: new ObjectID(req.params.id) }, updateDoc, function(err, doc) {
+    // Update only selected fields using $set operator:
+    db.collection(PLACES_COLLECTION).updateOne({ _id: new ObjectID(req.params.id) }, { $set: updateDoc }, function(err, doc) {
         if (err) {
             handleErrr(res, err.message, "Failed to update place");
+        } else if (false) {
+            // failed to update
         } else {
-            res.status(204).end();
+            // send updated place
+            res.status(204).json(doc);
         }
     });
 });
@@ -115,122 +118,11 @@ app.delete("/places/:id", function (req, res) {
     db.collection(PLACES_COLLECTION).deleteOne({ _id: new ObjectID(req.params.id) }, function (err, result) {
         if (err) {
             handleError(res, err.mesage, "Failed to delete place");
+        } else if (1) {
+            // failed to update
         } else {
+            // put in success message here?
             res.status(204).end();
         }
     });
 });
-
-/*
-// Test route to makes sure server is up (GET http://localhost:3000/)
-server.get('/', function (req, res, next) {
-	console.log("GET '/'");
-    res.writeHead(200, {
-        'Content-Type': 'application/json'
-    });
-    res.end('Server is up!');
-    return next();
-});
-
-// Get all the places (GET http://localhost:3000/places)
-server.get('places', function (req, res, next) {
-    console.log("GET /places");
-    db.places.find(function (err, places) {
-        res.writeHead(200, {
-            'Content-Type': 'application/json'
-        });
-        res.end(JSON.stringify(places));
-    });
-    return next();
-});
-
-// Get a single place (GET http://localhost:3000/place/<id>)
-server.get('/place/:_id', function (req, res, next) {
-    console.log('GET /place/' + req.params._id);
-    db.places.findOne({
-        _id: ObjectID.createFromHexString(req.params._id)
-    }, function (err, data) {
-        console.log(JSON.stringify(data));
-        res.writeHead(200, {
-            'Content-Type': 'application/json'
-        });
-        res.end(JSON.stringify(data));
-    });
-    return next();
-});
-
-// Create a new place (POST http://localhost:3000/places)
-server.post('/places', function (req, res, next) {
-    console.log('POST /places');
-    var place = req.body;
-    db.places.save(place,
-        function (err, data) {
-            res.writeHead(200, {
-                'Content-Type': 'application/json'
-            });
-            res.end(JSON.stringify(data));
-        });
-    return next();
-});
-
-// Update existing place (PUT http://localhost:3000/put/<name>)
-/* ----- /place PUT endpoint -----
-Effect: update existing place
-req.body.name = name of the place to update
-server.put('/place/:name', function (req, res, next) {
-    console.log('PUT /place/' + req.params.name);
-    // get existing place
-    db.places.findOne({
-        name: req.params.name
-    }, function (err, data) {
-        // Server isn't aware of changes;
-        // merge req.params/place with existing server/place
-        var updPlace = {};
-        for (var i in data) {
-            updPlace[i] = data[i];
-        }
-        for (var i in req.body) {
-            updPlace[i] = req.body[i];
-        }
-        db.places.update({
-            name: req.params.name
-        }, updPlace, {
-            multi: false
-        }, function (err, data) {
-            res.writeHead(200, {
-                'Content-Type': 'application/json'
-            });
-            res.end(JSON.stringify(data));
-        });
-    });
-    return next();
-}); */
-
-/* USING EXPRESS ROUTER AND MONGOOSE:
-/almanac/places routes -------------
-router.route('/places')
-    // Get all the places (GET http://localhost:3000/almanac/places)
-    .get(function (req, res) {
-        Place.find(function (err, places) {
-            if (err)
-                res.send(err);
-
-            res.json(places);
-        });
-    })
-
-    // Create a new place (POST http://localhost:3000/almanac/places)
-    .post(function (req, res) {
-        console.log('POST almanac/places');
-        var place = new Place(); // new instance of Place model
-        place.name = req.body.name;
-        console.log(place.name);
-
-        // If no errors, save the place
-        place.save(function (err) {
-            if (err)
-                res.send(err);
-
-            res.json({ message: 'Place created!' });
-        });
-    }); */
