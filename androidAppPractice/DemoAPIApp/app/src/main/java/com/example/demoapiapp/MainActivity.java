fdf86cbdc823061895c325673df8839018fb07cb
Kendra Swafford <kendra.swafford@hp.com>
2016-11-03 14:36:52 -0700

    More cleaning

--- androidAppPractice/DemoAPIApp/app/src/main/java/com/example/demoapiapp/MainActivity.java
+++ androidAppPractice/DemoAPIApp/app/src/main/java/com/example/demoapiapp/MainActivity.java
@@ -1,13 +1,87 @@
 package com.example.demoapiapp;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText emailText;
+    TextView responseView;
+    ProgressBar progressBar;
+    static final String API_KEY = "f34edbb1dc997da9";
+    static final String API_URL = "https://api.fullcontact.com/v2/person.json?";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        responseView = (TextView) findViewById(R.id.responseView);
+        emailText = (EditText) findViewById(R.id.emailText);
+        progressBar = (ProgressBar) findViewById(R.id.progressBar);
+
+        Button queryButton = (Button) findViewById(R.id.queryButton);
+        /*queryButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new RetrieveFeedTask().execute();
+            }
+        });*/
     }
+/*
+    class RetrieveFeedTask extends AsyncTask<Void, Void, String> {
+        private Exception exception;
+        String email = emailText.getText().toString();
+
+        protected void onPreExecute() {
+            progressBar.setVisibility(View.VISIBLE);
+            responseView.setText("");
+        }
+
+        protected String doInBackground(Void... urls) {
+            try {
+                URL url = new URL(API_URL + "email=" + email + "&apiKey=" + API_KEY);
+                HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+                try {
+                    BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
+                    StringBuilder stringBuilder = new StringBuilder();
+                    String line;
+                    while ((line = bufferedReader.readLine()) != null) {
+                        stringBuilder.append(line).append("\n");
+                    }
+                    bufferedReader.close();
+                    return stringBuilder.toString();
+                }
+                finally{
+                    urlConnection.disconnect();
+                }
+            }
+            catch(Exception e) {
+                Log.e("ERROR", e.getMessage(), e);
+                return null;
+            }
+        }
+
+        protected void onPostExecute(String response) {
+            if (response == null) {
+                response = "ERROR!";
+            }
+            progressBar.setVisibility(View.GONE);
+            Log.i("INFO", response);
+            responseView.setText(response);
+        }
+    }
+    */
 }
