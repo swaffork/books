f24816cf181f0a488ea2fb7612c1c2d441f7e606
Kendra Swafford <kendra.swafford@hp.com>
2016-11-06 13:26:08 -0800

    Now for the geolocation

---
+++ androidAppPractice/weatherApp/app/src/main/java/com/example/myfirstapp/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.myfirstapp;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+    public final static String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    /* Called when he the user clicks the Send button
+    * Note this method MUST be public, have a void return value, and have view
+    * as the only parameter. */
+    public void sendMessage(View view) {
+        /* Intent is object that provides runtime binding between separate
+         * components (like two activities). Represents an app's "intent to do
+         * something."
+         *  this: Context parameter (Activity is subclass of Context)
+         *  .class: the class of app component where the system will deliver the Intent*/
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = (EditText) findViewById(R.id.edit_message);
+        String message = editText.getText().toString();
+        /* adds the EditText's value to the intent; we define the key as
+         * EXTRA_MESSAGE to retrieve the text value. */
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+    }
+}
