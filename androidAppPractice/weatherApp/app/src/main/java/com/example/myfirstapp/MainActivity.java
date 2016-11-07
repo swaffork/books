48edbf6f4332b8b64ec401b9021285865e3bbdf6
Kendra Swafford <kendra.swafford@hp.com>
2016-11-06 22:17:11 -0800

    Something not working with img, taking out for now

--- androidAppPractice/weatherApp/app/src/main/java/com/example/myfirstapp/MainActivity.java
+++ androidAppPractice/weatherApp/app/src/main/java/com/example/myfirstapp/MainActivity.java
@@ -4,34 +4,78 @@ import com.example.myfirstapp.Location;
 import com.example.myfirstapp.Weather;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import org.json.JSONException;
+
+import static com.example.myfirstapp.R.id.condDescr;
+
+// *** not extends Activity?
 public class MainActivity extends AppCompatActivity {
     public final static String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
+    private TextView cityText;
+    private TextView condDesc;
+    private TextView temp;
+    private ImageView imgView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        String city = "London, UK";
+
+        cityText = (TextView) findViewById(R.id.cityText);
+        condDesc = (TextView) findViewById(R.id.condDescr);
+        temp = (TextView) findViewById(R.id.temp);
+        imgView = (ImageView) findViewById(R.id.condIcon);
+
+        // Async call to OpenWeather:
+        JSONWeatherTask task = new JSONWeatherTask();
+        task.execute(new String[]{city});
     }
 
-    /* Called when he the user clicks the Send button
-    * Note this method MUST be public, have a void return value, and have view
-    * as the only parameter. */
-    public void sendMessage(View view) {
-        /* Intent is object that provides runtime binding between separate
-         * components (like two activities). Represents an app's "intent to do
-         * something."
-         *  this: Context parameter (Activity is subclass of Context)
-         *  .class: the class of app component where the system will deliver the Intent*/
-        Intent intent = new Intent(this, DisplayMessageActivity.class);
-        EditText editText = (EditText) findViewById(R.id.edit_message);
-        String message = editText.getText().toString();
-        /* adds the EditText's value to the intent; we define the key as
-         * EXTRA_MESSAGE to retrieve the text value. */
-        intent.putExtra(EXTRA_MESSAGE, message);
-        startActivity(intent);
+    /*@Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }*/
+
+    private class JSONWeatherTask extends AsyncTask<String, Void, Weather> {
+
+        @Override
+        protected Weather doInBackground(String... params) {
+            Weather weather = new Weather();
+            String data = ((new WeatherHttpClient()).getWeatherData(params[0]));
+
+            try {
+                weather = JSONWeatherParser.getWeather(data);
+                weather.iconData = ((new WeatherHttpClient()).getImage(weather.currentCondition.getIcon()));
+            }
+            catch (JSONException e) {
+                e.printStackTrace();
+            }
+            return weather;
+        }
+
+        @Override
+        protected void onPostExecute(Weather weather) {
+            super.onPostExecute(weather);
+
+            if ( (weather.iconData != null) && (weather.iconData.length > 0) ) {
+                Bitmap image = BitmapFactory.decodeByteArray(weather.iconData, 0, weather.iconData.length); // data, offset, length
+                cityText.setText(weather.location.getCity() + "," + weather.location.getCountry()); // City, Country
+                condDesc.setText(weather.currentCondition.getCondition() + "-" + weather.currentCondition.getDescr()); // Condition description
+                temp.setText("" + Math.round((weather.temperature.getTemp() - 273.15)) + "Celsius");
+            }
+        }
     }
 }
