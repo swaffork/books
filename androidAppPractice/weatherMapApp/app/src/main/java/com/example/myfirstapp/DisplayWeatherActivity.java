89c22dc00c721e897c652b42193d13a0adb1cafa
Kendra Swafford <kendra.swafford@hp.com>
2016-11-07 18:51:59 -0800

    have all scaffolding, about to test weatherMapApp, may add onClick listener from DemoAPI

--- androidAppPractice/weatherMapApp/app/src/main/java/com/example/myfirstapp/DisplayWeatherActivity.java
+++ androidAppPractice/weatherMapApp/app/src/main/java/com/example/myfirstapp/DisplayWeatherActivity.java
@@ -1,32 +1,70 @@
 package com.example.myfirstapp;
 
 import android.content.Intent;
-import android.support.v4.view.ViewGroupCompat;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import org.json.JSONException;
+
 public class DisplayWeatherActivity extends AppCompatActivity {
+    private TextView cityText;
+    private TextView condDesc;
+    private TextView temp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_display_message);
+        setContentView(R.layout.activity_display_weather);
 
         // Grab the intent that started the activity:
         Intent intent = getIntent();
-        // Retrieve data from first activity:
         String location = intent.getStringExtra(MainActivity.LOCATION);
-        // Programmatically create a TextView, set its size & message:
+
+        /* Programmatically create a TextView, set its size & message:
         TextView textView = new TextView(this);
         textView.setTextSize(40);
         textView.setText(location);
-
-        ViewGroup layout = (ViewGroup) findViewById(R.id.activity_display_message);
+        ViewGroup layout = (ViewGroup) findViewById(R.id.activity_display_weather);
         // Add the TextView to the layout:
-        layout.addView(textView);
+        layout.addView(textView); */
+
+        // Populate results?
+        cityText = (TextView) findViewById(R.id.cityText);
+        condDesc = (TextView) findViewById(R.id.condDescr);
+        temp = (TextView) findViewById(R.id.temp);
+        JSONWeatherTask task = new JSONWeatherTask();
+        task.execute(new String[]{location});
     }
 
+    private class JSONWeatherTask extends AsyncTask<String, Void, Weather> {
+
+        @Override
+        protected Weather doInBackground(String... params) {
+            Weather weather = new Weather();
+            String data = ((new WeatherHttpClient()).getWeatherData(params[0]));
+
+            try {
+                weather = JSONWeatherParser.getWeather(data);
+                //weather.iconData = ((new WeatherHttpClient()).getImage(weather.currentCondition.getIcon()));
+            }
+            catch (JSONException e) {
+                e.printStackTrace();
+            }
+            return weather;
+        }
+
+        @Override
+        protected void onPostExecute(Weather weather) {
+            super.onPostExecute(weather);
+
+            cityText.setText(weather.location.getCity() + ", " + weather.location.getCountry()); // City, Country
+            condDesc.setText(weather.currentCondition.getCondition() + " - " + weather.currentCondition.getDescr()); // Condition description
+            temp.setText((Math.round((weather.temperature.getTemp() - 273.15))) + " degrees C");
+            //}
+        }
+    }
 
 }
---
+++ androidAppPractice/weatherMapApp/app/src/main/java/com/example/myfirstapp/WeatherHttpClient.java
@@ -0,0 +1,57 @@
+package com.example.myfirstapp;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class WeatherHttpClient {
+
+    private static String API_URL = "http://api.openweathermap.org/data/2.5/weather?q=";
+    private static String API_KEY = "978c1079e8f399666c17af2e9c092eaf";
+
+    public String getWeatherData(String location) {
+        URL url = null;
+        HttpURLConnection connection = null;
+        InputStream is = null;
+
+        try {
+            // Make the request:
+            String urlString = API_URL + location + "&APPID=" + API_KEY;
+            url = new URL(urlString);
+            connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("POST");
+            connection.setDoInput(true);
+            connection.setDoOutput(true);
+            connection.connect();
+
+            // Read the response:
+            StringBuffer buffer = new StringBuffer();
+            is = connection.getInputStream();
+            BufferedReader br = new BufferedReader(new InputStreamReader(is));
+            String line = null;
+            while ( (line = br.readLine()) != null ) {
+                buffer.append(line + "\r\n");
+            }
+            is.close();
+            connection.disconnect();
+            return buffer.toString();
+        }
+        catch(Throwable t) {
+            t.printStackTrace();
+        }
+        // Clean up:
+        finally {
+            try {
+                is.close();
+            }
+            catch(Throwable t) {}
+            try {
+                connection.disconnect();
+            }
+            catch(Throwable t) {}
+        }
+        return null;
+    }
+}
