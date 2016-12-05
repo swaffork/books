a13d2cfca2323bf1a408de70ac3aa6e2a506fdd9
Kendra Swafford <kendra.swafford@hp.com>
2016-12-04 16:44:59 -0800

    experimenting with alternative to geolocation because no https

--- final/app/geoscript.js
+++ final/app/geoscript.js
@@ -1,28 +1,11 @@
+var request = require('request');
+
 function geoFindMe() {
-  var output = document.getElementById("out");
+    var output = document.getElementById("out");
 
-  if (!navigator.geolocation){
-    output.innerHTML = "<p>Geolocation is not supported by your browser</p>";
-    return;
-  }
-
-  function success(position) {
-    var latitude  = position.coords.latitude;
-    var longitude = position.coords.longitude;
-
-    output.innerHTML = '<p>Latitude is ' + latitude + '° <br>Longitude is ' + longitude + '°</p>';
-
-    var img = new Image();
-    img.src = "https://maps.googleapis.com/maps/api/staticmap?center=" + latitude + "," + longitude + "&zoom=13&size=300x300&sensor=false";
-
-    output.appendChild(img);
-  }
-
-  function error(error) {
-    output.innerHTML = "Unable to retrieve your location" + error.code + " - " + error.message;
-  }
-
-  output.innerHTML = "<p>Locating…</p>";
-
-  navigator.geolocation.getCurrentPosition(success, error);
-}
+	request('http://ip-api.com/json', function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+        console.log(body.city);
+		output.innerHTML = '<p>Latitude is ' + body.lat + '° <br>Longitude is ' + body.lon + '°</p>';
+    }
+});
