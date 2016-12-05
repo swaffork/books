e4cd42f863c0280af1af649623f76dd3328b5c2a
Kendra Swafford <kendra.swafford@hp.com>
2016-12-04 16:49:58 -0800

    fixed typo again

--- final/app/geoscript.js
+++ final/app/geoscript.js
@@ -3,7 +3,7 @@ var request = require('request');
 function geoFindMe() {
     var output = document.getElementById("out");
 
-	request('http://ip-api.com/json', function (error, response, body) {
+	request('http://ip-api.com/json', function (error, response, body)) {
 		if (!error && response.statusCode == 200) {
 			console.log(body.city);
 			output.innerHTML = '<p>Latitude is ' + body.lat + '° <br>Longitude is ' + body.lon + '°</p>';
