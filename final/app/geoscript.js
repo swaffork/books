c7384ff842b146e487e0fa70df3152063d011430
Kendra Swafford <kendra.swafford@hp.com>
2016-12-04 17:13:36 -0800

    clean

--- final/app/geoscript.js
+++ final/app/geoscript.js
@@ -7,11 +7,11 @@ function geoFindMe() {
 	xhr.send();
 	
 	xhr.addEventListener("readystatechange", processRequest, false);
-}
-
-function processRequest(e) {
-    if (xhr.readyState == 4 && xhr.status == 200) {
-        var response = JSON.parse(xhr.responseText);
-        alert(response.ip);
-    }
+	
+	function processRequest(e) {
+		if (xhr.readyState == 4 && xhr.status == 200) {
+			var response = JSON.parse(xhr.responseText);
+			alert(response.ip);
+		}
+	}
 }
